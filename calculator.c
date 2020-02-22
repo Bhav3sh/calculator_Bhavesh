ba109033f86a700753d1bcdd556126e12b19de18
nseit <nseit@localhost.localdomain>
2020-02-22 03:58:24 -0500

    fist execution

---
+++ calculator.c
@@ -0,0 +1,7 @@
+void main(){
+
+int a=10;b=20;
+
+printf("after addition %d"&(a+b));
+
+}
---
+++ calculator.java
@@ -0,0 +1,11 @@
+Class Calculator{
+
+public static void main(String[]){
+
+int a=10,b=20,x;
+
+x=a+b;
+
+system.out.println(+x);
+
+}
